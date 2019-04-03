2a021d54fb07ba3b44434a0052ed0aa44c180808
Tomohiro Yoshida <tomohiro@ty-mbp.local>
2019-04-03 23:59:48 +0900

    add: Q14 Project

---
+++ Q14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using BashExtension;
+
+namespace Q14
+{
+    class Program
+    {
+        const string filePath = "../hightemp.txt";
+
+        static void Main(string[] args)
+        {
+            int count = 2;
+            Console.WriteLine(GetHeadNLinesText(filePath, count));
+
+            // Bash
+            $"head -n {count} {filePath}".WriteBashLine();
+        }
+
+        static string GetHeadNLinesText(string textFilePath, int n)
+        {
+            return string.Join("\n", GetHeadNLines(textFilePath, n));
+        }
+
+        static IEnumerable<string> GetHeadNLines(string textFilePath, int n)
+        {
+            using (var sr = new StreamReader(filePath, Encoding.Default))
+            {
+                foreach (var i in Enumerable.Range(1, n))
+                {
+                    yield return sr.ReadLine();
+                }
+            }
+        }
+    }
+}
