fc07323a0363038acab7ff451ba35d101af04c4c
Tomohiro Yoshida <tomohiro@ty-mbp.local>
2019-04-03 23:42:39 +0900

    add: Q13 project

---
+++ Q13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using BashExtension;
+
+namespace Q13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var col1Lines = File.ReadAllLines("../col1.txt");
+            var col2Lines = File.ReadAllLines("../col1.txt");
+
+            var mergedContent = string.Join("\n", col1Lines.Zip(col2Lines, (l1, l2) => $"{l1}\t{l2}"));
+
+            File.WriteAllText("./merged.txt", mergedContent);
+
+            // 確認用
+            "paste ../col1.txt ../col2.txt".WriteBashLine();
+        }
+    }
+}
