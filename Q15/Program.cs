ba5d90abc7742a55bc4bc75a7bf47f9cc185d40f
Tomohiro Yoshida <tomohiro@ty-mbp.local>
2019-04-09 23:20:36 +0900

    add 15, 16

---
+++ Q15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using BashExtension;
+
+namespace Q15
+{
+    class Program
+    {
+        const string filePath = "../hightemp.txt";
+
+        static void Main(string[] args)
+        {
+            int num = 3;
+            Console.WriteLine(GetTailNLinesText(filePath, num));
+
+            // Bash
+            $"tail -n {num} {filePath}".WriteBashLine();
+        }
+
+        static string GetTailNLinesText(string path, int num)
+        {
+            IEnumerable<string> f(string p, int n)
+            {
+                return File.ReadAllLines(p)
+                    .TakeLast(n);
+            }
+
+            return string.Join("\n", f(path, num));
+        }
+    }
+}
---
+++ Q16/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LogExtension;
+using BashExtension;
+
+namespace Q16
+{
+    class Program
+    {
+        const string filePath = "../hightemp.txt";
+
+        static void Main(string[] args)
+        {
+            int num = 7;
+            var ret = GetSplittedLines(File.ReadAllLines(filePath), num);
+            ret.DebugLog("splitted");
+
+            $"split -l {num} {filePath} q16_output".Bash();
+        }
+
+        static IEnumerable<List<string>> GetSplittedLines(IEnumerable<string> list, int n)
+        {
+            Func<int, Func<int>> f1 = x => () => x++;
+            var i = f1(0);
+
+            while (true)
+            {
+                var ret = list.Skip(i() * n).Take(n).ToList();
+
+                if (ret.Any())
+                {
+                    yield return ret;
+                }
+
+                if (ret.Count < n)
+                {
+                   yield break;
+                }
+            }
+        }
+    }
+}
